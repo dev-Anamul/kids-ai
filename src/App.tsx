b2c524ce012c00f34d757bb19516a2a904653ce9
developerachem <developerachem@gmail.com>
2024-12-04 23:00:11 +0600

    remove text

--- src/App.tsx
+++ src/App.tsx
@@ -127,62 +127,7 @@ function App() {
             lineHeight: 1.2,
           }}
         >
-          {data} Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
-          quae eligendi tempore quidem beatae sed quisquam nostrum reiciendis
-          ad? Nisi recusandae repudiandae enim dolore quisquam nam facilis
-          perspiciatis quae ducimus molestias. Recusandae hic, unde sed atque
-          odit illo amet quidem! Perferendis voluptas sapiente culpa omnis esse.
-          Impedit aut adipisci recusandae qui voluptatum unde, perferendis nemo
-          ut doloribus eum iure ab accusamus sed, obcaecati, quia tempore
-          praesentium suscipit quis consequatur nesciunt eaque! Totam iste sit
-          porro fuga ab veniam at quam blanditiis ipsam! Ex ipsa molestias
-          veritatis iste. Natus rerum autem veritatis, consequatur praesentium
-          esse molestiae, amet omnis eligendi nam architecto sequi, perferendis
-          inventore distinctio aliquam vel recusandae laboriosam totam ipsum
-          sint accusamus? Adipisci facere iure laboriosam error impedit suscipit
-          neque! Commodi facere repellendus, pariatur corrupti aliquid modi,
-          iusto itaque reprehenderit iste architecto nemo consectetur aliquam,
-          illo libero voluptatem. Unde alias assumenda vero cum natus asperiores
-          recusandae magnam ex ad quas sint necessitatibus, dolorum error
-          doloribus nisi rerum? Iste, odio blanditiis incidunt aspernatur
-          voluptas, ut quisquam reiciendis ad dicta facere, ipsa laborum
-          nesciunt debitis eos maxime reprehenderit harum. At labore ipsam
-          quaerat debitis nulla doloribus architecto ad sequi odio libero
-          accusamus excepturi, dolore voluptate qui laudantium quia iure dicta
-          quis tempora, voluptatibus praesentium repellat sit nisi officia.
-          Earum, fugit, natus deleniti ullam praesentium hic quisquam corrupti
-          ipsam voluptatem veritatis iure nesciunt fuga asperiores. Deserunt
-          eligendi voluptates praesentium possimus quibusdam repudiandae ipsa
-          officia reiciendis? Soluta sed aliquam, magni nobis cupiditate omnis
-          assumenda excepturi explicabo ipsum corrupti quam quae veritatis
-          laboriosam modi doloribus id reiciendis nihil pariatur minus vel
-          voluptatum! Vero, animi. Recusandae suscipit, in, architecto similique
-          minima fugiat pariatur labore fugit nulla veritatis aspernatur
-          asperiores autem quas esse quos? Neque a harum pariatur quaerat vero
-          illum autem repellat, ipsa eum fuga! Eligendi ipsam unde cupiditate
-          voluptatum iure libero aut, minus ipsa. Saepe quos, optio eum id ad,
-          omnis alias ullam necessitatibus accusamus, iusto harum ut temporibus!
-          Vero, hic. Accusamus dolorem similique, laboriosam ad, error dolorum
-          commodi assumenda itaque vitae fuga sit, cum odit. Vero, illum fugiat.
-          Sunt, corporis perferendis? Laboriosam facere excepturi id eius ipsum,
-          nemo magni nisi doloremque vero et. Nihil ea quis rem dolorum harum
-          illo saepe. Voluptatem adipisci officiis quibusdam porro deserunt
-          consequatur laborum natus quasi vero veritatis, rerum velit sint
-          beatae in odio, debitis, ut laudantium quod fugit quidem minus. Itaque
-          distinctio consequuntur enim voluptates fuga hic est, omnis illo,
-          incidunt placeat non! Dolore maxime laborum voluptas consequatur
-          officia, quas, sint totam obcaecati dicta repellendus pariatur ullam
-          commodi nihil placeat blanditiis temporibus quisquam beatae enim
-          nostrum. Possimus ad fugit suscipit sint porro aut harum similique ex
-          voluptate. Asperiores praesentium quibusdam iusto tenetur optio
-          laborum blanditiis et ratione quod corrupti possimus doloribus,
-          voluptatibus magni ad perspiciatis sit doloremque omnis dicta nemo
-          iste placeat facere accusantium hic nulla? Labore iste error commodi
-          magnam, adipisci molestiae consectetur praesentium repudiandae
-          ratione, dolor vel, rerum quod eius id. Quos dolores harum accusantium
-          quis pariatur ipsa repudiandae! Quos, id quia atque obcaecati
-          repudiandae praesentium! Dicta expedita quisquam enim porro
-          perferendis laboriosam fuga alias neque.
+          {data}
         </p>
       )}
     </div>
